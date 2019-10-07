cd1a5f5b8e97b885e0ab9bc53b754995cc80ee23
Francesco Rivola <francesco.rivola@gmail.com>
2019-10-07 22:46:37 +0200

    added schema for list and create endpoint

--- __tests__/routes/events.js
+++ __tests__/routes/events.js
@@ -13,14 +13,43 @@ describe('admin', () => {
 
     describe('events', () => {
 
-        it('should return 200 with array of events', async () => {
-            const response = await server.inject({
-                method: 'GET',
-                url: '/admin/events'
+        describe('get', () => {
+            it('should return 200 with array of events', async () => {
+                const response = await server.inject({
+                    method: 'GET',
+                    url: '/admin/events'
+                });
+                expect(response.statusCode).toBe(200);
+                expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+                expect(response.payload).toBe(JSON.stringify({ items: [] }));
             });
-            expect(response.statusCode).toBe(200);
-            expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
-            expect(response.payload).toBe(JSON.stringify([]));
+        });
+
+        describe('post', () => {
+            it('should return 400 when name is undefined', async () => {
+                const response = await server.inject({
+                    method: 'POST',
+                    url: '/admin/events',
+                    body: {
+                        name: undefined
+                    }
+                });
+                expect(response.statusCode).toBe(400);
+                expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+                expect(response.payload).toBe(JSON.stringify({ statusCode: 400, error: 'Bad Request', message: 'body should have required property \'name\'' }));
+            });
+
+            it('should return 201 with created event when request is valid', async () => {
+                const response = await server.inject({
+                    method: 'POST',
+                    url: '/admin/events',
+                    body: {
+                        name: 'sensor-data'
+                    }
+                });
+                expect(response.statusCode).toBe(201);
+                expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+            });
         });
     });
 });
--- src/routes/admin/events.js
+++ src/routes/admin/events.js
@@ -1,10 +1,53 @@
 
 async function list() {
-    return [];
+    return { items: [] };
 }
 
+function create(request, reply) {
+    const { name } = request.body;
+    reply.status(201).send({ name });
+}
+
+const eventSchema = {
+    type: 'object',
+    properties: {
+        name: { type: 'string' },
+        id: { type: 'string' },
+        url: { type: 'string' },
+        createAt: { type: 'string' }
+    }
+};
+
+const listSchema = {
+    response: {
+        200: {
+            type: 'object',
+            properties: {
+                items: {
+                    type: 'array',
+                    items: eventSchema
+                }
+            }
+        }
+    }
+};
+
+const createSchema = {
+    body: {
+        type: 'object',
+        required: ['name'],
+        properties: {
+            name: { type: 'string' }
+        }
+    },
+    response: {
+        201: eventSchema
+    }
+};
+
 export default function(fastify, opts, next) {
-    fastify.get('/', opts, list);
+    fastify.get('/', { ...opts, schema: listSchema }, list);
+    fastify.post('/', { ...opts, schema: createSchema }, create);
     next();
 }
 
