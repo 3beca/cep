b37ffb68548b44fa2f3d1f7b020722ae3edca2d2
Francesco Rivola <francesco.rivola@gmail.com>
2019-10-09 23:13:43 +0200

    added geolib

---
+++ __tests__/geolib.js
@@ -0,0 +1,40 @@
+import * as geolib from 'geolib';
+
+describe('geolib', function() {
+
+    describe('getDistance', function() {
+
+        it('should return NaN when coordinates inputs are invalid', function() {
+            const coord1 = { someOtherInvalidField: 2};
+            const coord2 = { someInvalidField: 1};
+
+            const distance = geolib.getDistance(coord1, coord2);
+
+            expect(distance).toBe(NaN);
+        });
+
+        it('should thrown an Error when coordinates inputs are null', function() {
+            function act() { geolib.getDistance(null, null); }
+
+            expect(act).toThrow();
+        });
+
+        it('should return distance from Murcia to Lorca in meters', function() {
+            const coord1 = { latitude: 38.002656, longitude: -1.164551 };
+            const coord2 = { latitude: 37.668603, longitude: -1.711121 };
+
+            const distance = geolib.getDistance(coord1, coord2);
+
+            expect(distance).toBe(60761);
+        });
+
+        it('should return distance from Murcia to Modigliana in meters', function() {
+            const coord1 = { latitude: 38.002656, longitude: -1.164551 };
+            const coord2 = { latitude: 44.158537, longitude: 11.792105 };
+
+            const distance = geolib.getDistance(coord1, coord2);
+
+            expect(distance).toBe(1282849);
+        });
+    });
+});
