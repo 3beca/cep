bd0f9d2fdc2cae5eb2426b6ebeb264e3831b5f31
Francesco Rivola <francesco.rivola@gmail.com>
2019-10-09 22:21:13 +0200

    fixed getExternalUrl if path have end slash

---
+++ __tests__/utils/url.js
@@ -0,0 +1,22 @@
+import { getExternalUrl } from '../../src/utils/url';
+
+describe('url', () => {
+
+    describe('getExternalUrl()', () => {
+
+        it('should remove end slash if present', () => {
+            const result = getExternalUrl('/my-path/');
+            expect(result).toBe('http://localhost:8888/my-path');
+        });
+
+        it('should not modify path if end slash is not present', () => {
+            const result = getExternalUrl('/my-path');
+            expect(result).toBe('http://localhost:8888/my-path');
+        });
+
+        it('should return external url when path is undefined', () => {
+            const result = getExternalUrl();
+            expect(result).toBe('http://localhost:8888');
+        });
+    });
+});
--- src/utils/url.js
+++ src/utils/url.js
@@ -3,19 +3,26 @@ import url from 'url';
 
 const { externalHttp } = config;
 
+function removeEndSlash(path) {
+    if (path[path.length - 1] === '/') {
+        return path.slice(0, -1);
+    }
+    return path;
+}
+
 export function getExternalUrl(path) {
     const { protocol, host, port } = externalHttp;
-    return `${protocol}://${host}${port ? ':' + port : ''}${path}`;
+    return `${protocol}://${host}${port ? ':' + port : ''}${path ? removeEndSlash(path) : ''}`;
 }
 
 export function getPrevLink(request) {
     const { page, pageSize } = request.query;
     const path = url.parse(request.raw.url).pathname;
-    return page !== 1 ? getExternalUrl(`${path}?page=${page - 1}&pageSize=${pageSize}`) : undefined;
+    return page !== 1 ? `${getExternalUrl()}${path}?page=${page - 1}&pageSize=${pageSize}` : undefined;
 }
 
 export function getNextLink(request, results) {
     const { page, pageSize } = request.query;
     const path = url.parse(request.raw.url).pathname;
-    return results.length === pageSize ? getExternalUrl(`${path}?page=${page + 1}&pageSize=${pageSize}`) : undefined;
+    return results.length === pageSize ? `${getExternalUrl()}${path}?page=${page + 1}&pageSize=${pageSize}` : undefined;
 }
