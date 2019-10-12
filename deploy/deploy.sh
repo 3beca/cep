2f138c8dd79126bfdc5987d8436c3fca982bb3e0
Francesco Rivola <francesco.rivola@gmail.com>
2019-10-12 16:28:10 +0200

    change helm image variable and http host bind 0.0.0.0

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,7 +3,7 @@ set -e
 echo Deploy image $IMAGE_NAME:$CI_TIMESTAMP
 scp -r ./var/www/deploy/charts/** $USER_NAME@$HOST_NAME:./charts/tribeca-cep/
 ssh $USER_NAME@$HOST_NAME helm upgrade tribeca-cep ./charts/tribeca-cep \
-    --set image.tag=$IMAGE_NAME:$CI_TIMESTAMP \
+    --set image=$IMAGE_NAME:$CI_TIMESTAMP \
     --set deployment.env.externalHttpHost=$EXTERNAL_HTTP_HOST \
     --install --wait
 
