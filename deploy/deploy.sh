99244024299921f5b72963e81b9cf37c6daf8a86
Francesco Rivola <francesco.rivola@gmail.com>
2019-10-12 15:44:44 +0200

    fix scp -f

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo Deploy image $IMAGE_NAME:$CI_TIMESTAMP
-scp -v ./deploy/charts $USER_NAME@$HOST_NAME:/charts/cep
+scp -v -r ./deploy/charts $USER_NAME@$HOST_NAME:/charts/cep
 ssh -v $USER_NAME@$HOST_NAME helm upgrade tribeca-cep ./charts/cep \
     --set image.tag=$IMAGE_NAME:$CI_TIMESTAMP \
     --set deployment.env.externalHttpHost=$EXTERNAL_HTTP_HOST \
