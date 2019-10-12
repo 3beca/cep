90018630743f121f65d45961c35fcdd831aaf207
Francesco Rivola <francesco.rivola@gmail.com>
2019-10-12 15:53:33 +0200

    attempt #2 to fix the deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 echo Deploy image $IMAGE_NAME:$CI_TIMESTAMP
-ls
-scp -v -r ./charts/** $USER_NAME@$HOST_NAME:./charts/$IMAGE_NAME/
+scp -v -r ./var/www/deploy/charts/** $USER_NAME@$HOST_NAME:./charts/$IMAGE_NAME/
 ssh -v $USER_NAME@$HOST_NAME helm upgrade $IMAGE_NAME ./charts/$IMAGE_NAME \
     --set image.tag=$IMAGE_NAME:$CI_TIMESTAMP \
     --set deployment.env.externalHttpHost=$EXTERNAL_HTTP_HOST \
