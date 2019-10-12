59d9cb8291cc39af008261c7f4d5e22b19c3dff6
Francesco Rivola <francesco.rivola@gmail.com>
2019-10-12 16:03:05 +0200

    removed verbose flag from deploy sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 echo Deploy image $IMAGE_NAME:$CI_TIMESTAMP
-scp -v -r ./var/www/deploy/charts/** $USER_NAME@$HOST_NAME:./charts/tribeca-cep/
-ssh -v $USER_NAME@$HOST_NAME helm upgrade tribeca-cep ./charts/tribeca-cep \
+scp -r ./var/www/deploy/charts/** $USER_NAME@$HOST_NAME:./charts/tribeca-cep/
+ssh $USER_NAME@$HOST_NAME helm upgrade tribeca-cep ./charts/tribeca-cep \
     --set image.tag=$IMAGE_NAME:$CI_TIMESTAMP \
     --set deployment.env.externalHttpHost=$EXTERNAL_HTTP_HOST \
     --install --wait
