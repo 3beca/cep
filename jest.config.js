af561cea6f345caa12ec141414db1ac9e27e5904
Francesco Rivola <francesco.rivola@gmail.com>
2020-05-07 23:40:11 +0200

    increased jest timeout

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     testEnvironment: "node",
     coverageDirectory: "./coverage/",
     collectCoverage: true,
-    collectCoverageFrom: ["src/**/*.ts"]
+    collectCoverageFrom: ["src/**/*.ts"],
+    testTimeout: 10000
   };
   
