edab7b72654fbb2d7f02f23ba2d4d72ad9ef344b
Francesco Rivola <francesco.rivola@gmail.com>
2020-06-14 19:45:35 +0200

    add metrics expected entries

--- __tests__/metrics-server.ts
+++ __tests__/metrics-server.ts
@@ -27,12 +27,34 @@ describe('metrics server', () => {
     });
 
     it('should return 200 with prometheus metrics', async () => {
+        await app.getInternalServer().inject({
+            method: 'GET',
+            url: '/test'
+        });
+        await app.getServer().inject({
+            method: 'GET',
+            url: '/test'
+        });
         const response = await metricsServer.inject({
             method: 'GET',
             url: '/metrics'
         });
         expect(response.statusCode).toBe(200);
         expect(response.headers['content-type']).toBe('text/plain');
+        expect(response.payload).toContain('process_cpu_user_seconds_total');
+        expect(response.payload).toContain('nodejs_version_info');
+        expect(response.payload).toContain('cep_internal_server_http_request_duration_seconds_bucket');
+        expect(response.payload).toContain('cep_internal_server_http_request_duration_seconds_sum');
+        expect(response.payload).toContain('cep_internal_server_http_request_duration_seconds_count');
+        expect(response.payload).toContain('cep_internal_server_http_request_summary_seconds');
+        expect(response.payload).toContain('cep_internal_server_http_request_summary_seconds_sum');
+        expect(response.payload).toContain('cep_internal_server_http_request_summary_seconds_count');
+        expect(response.payload).toContain('cep_server_http_request_duration_seconds_bucket');
+        expect(response.payload).toContain('cep_server_http_request_duration_seconds_sum');
+        expect(response.payload).toContain('cep_server_http_request_duration_seconds_count');
+        expect(response.payload).toContain('cep_server_http_request_summary_seconds');
+        expect(response.payload).toContain('cep_server_http_request_summary_seconds_sum');
+        expect(response.payload).toContain('cep_server_http_request_summary_seconds_count');
     });
 
     it('should return 500 when unhandled errors happened', async () => {
