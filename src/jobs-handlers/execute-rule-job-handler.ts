462fe819d64315b382b87e45ae81bd703e8f1ac0
Francesco Rivola <francesco.rivola@gmail.com>
2020-07-17 17:47:27 +0200

    converted execute rule route test to job handler tests

--- src/jobs-handlers/execute-rule-job-handler.ts
+++ src/jobs-handlers/execute-rule-job-handler.ts
@@ -1,7 +1,9 @@
 import { Engine } from '../engine';
+import { JobHandler } from './job-handler';
+import { JobData } from './job-data';
 
-export default function buildExecuteRuleJobHandler(engine: Engine): (data) => Promise<void> {
-    return function executeRuleJobHandler(data) {
+export default function buildExecuteRuleJobHandler(engine: Engine): JobHandler {
+    return function executeRuleJobHandler(data: JobData) {
         return engine.executeRule(data.ruleId, 'TODO');
     };
 }
