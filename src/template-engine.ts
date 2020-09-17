1619ebe3e719504d0643f60b1fe71094e1f91a05
Francesco Rivola <francesco.rivola@gmail.com>
2020-09-18 00:06:30 +0200

    refactor template engine code

--- src/template-engine.ts
+++ src/template-engine.ts
@@ -17,40 +17,52 @@ export function buildTemplateEngine(): TemplateEngine {
         } as unknown as FS
     });
 
-    async function render(templateObject: object, model: any, baseKey: string = '/'): Promise<object> {
-        if (!templateObject || typeof templateObject !== 'object') {
+    function render(templateObject: object, model: any) : Promise<object> {
+        return renderObject(templateObject, model);
+    }
+
+    async function renderObject(object: object, model: any, baseKey: string = '/'): Promise<object> {
+        if (!object || typeof object !== 'object') {
             throw Error('template object must be an object');
         }
-        const keys = Object.keys(templateObject);
+        const keys = Object.keys(object);
         const result = {};
         for (const key of keys) {
-            result[key] = await renderValue(templateObject[key], model, baseKey + key);
+            result[key] = await renderValue(object[key], model, baseKey + key);
         }
         return result;
     }
 
     async function renderValue(value: object | string | number | [], model: any, baseKey: string): Promise<object | string | number | []> {
         if (typeof value === 'string' && value) {
-            if (canBeConvertedToFloat(value)) {
-                return value;
-            }
-            try {
-                const renderedValue = await engine.parseAndRender(value, model);
-                const renderedNumber = parseFloat(renderedValue);
-                return isNaN(renderedNumber) ? renderedValue : renderedNumber;
-            } catch (error) {
-                const { message } = error;
-                if (message.includes('ENOENT')) {
-                    throw Error(`${baseKey} partials and layouts are not supported${message.substring(message.indexOf(', line:'))}`);
-                }
-                throw Error(`${baseKey} ${message}`);
-            }
+            return renderString(value, model, baseKey);
         } else if (isArray(value)) {
-            return await Promise.all(value.map((v, i) => renderValue(v, model, `${baseKey}/${i}/`)));
+            return await renderArray(value, model, baseKey);
         } else if (value && typeof value === 'object') {
-            return await render(value, model, baseKey);
+            return await renderObject(value, model, baseKey);
         }
         return value;
+    }
+
+    async function renderString(value: string, model, baseKey: string): Promise<string | number> {
+        if (canBeConvertedToFloat(value)) {
+            return value;
+        }
+        try {
+            const renderedValue = await engine.parseAndRender(value, model);
+            const renderedNumber = parseFloat(renderedValue);
+            return isNaN(renderedNumber) ? renderedValue : renderedNumber;
+        } catch (error) {
+            const { message } = error;
+            if (message.includes('ENOENT')) {
+                throw Error(`${baseKey} partials and layouts are not supported${message.substring(message.indexOf(', line:'))}`);
+            }
+            throw Error(`${baseKey} ${message}`);
+        }
+    }
+
+    function renderArray(values: [], model, baseKey: string): Promise<(string | number | object | [])[]> {
+        return Promise.all(values.map((value, i) => renderValue(value, model, `${baseKey}/${i}/`)));
     }
 
     function canBeConvertedToFloat(value: string): boolean {
