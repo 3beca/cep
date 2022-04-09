24579d26483287d7fdaf49defd27be07a239ece7
Francesco Rivola <francesco.rivola@xepient.com>
2022-04-09 09:37:03 +0200

    upgraded eslint to latest

--- src/errors/conflict-error.ts
+++ src/errors/conflict-error.ts
@@ -1,6 +1,6 @@
 export default class ConflictError extends Error {
     public id: string;
-    public resources: string
+    public resources: string;
     constructor(message: string, id: string, resources: string) {
         super(message);
         this.id = id;
